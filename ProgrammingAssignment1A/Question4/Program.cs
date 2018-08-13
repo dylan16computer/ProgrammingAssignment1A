03946f6a6db6f3f9ff7e32b5ddb3f48e81c8ff30
Dylan <30002799@g.toiohomai.ac.nz>
2018-08-13 19:45:19 +1200

    Question 4 incomplete. Cannot loop with object.

--- ProgrammingAssignment1A/Question4/Program.cs
+++ ProgrammingAssignment1A/Question4/Program.cs
@@ -8,64 +8,34 @@ namespace Question4
 {
     class Program
     {
-        static int personCounter = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your first name: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter your last name: ");
-            string lastName = Console.ReadLine();
-            Person person1 = new Person(firstName, lastName);
-            Console.WriteLine("Enter your year of birth: ");
-            person1.YearOfBirth = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Full Name: {person1.GetFullName()}\nAge: {person1.GetAge()}");
-            Console.WriteLine("Would you like to add another person? y for yes, n for no.");
-            string decide = Console.ReadLine();
-            if(decide == "y")
+            var personList = new List<Person>();
+            string decide;
+            do
             {
-                Console.WriteLine("Enter your first name: ");
-                firstName = Console.ReadLine();
-                Console.WriteLine("Enter your last name: ");
-                lastName = Console.ReadLine();
-                Person person2 = new Person(firstName, lastName);
-                Console.WriteLine("Enter your year of birth: ");
-                person2.YearOfBirth = int.Parse(Console.ReadLine());
-                Console.WriteLine($"Full Name: {person2.GetFullName()}\nAge: {person2.GetAge()}");
-            }
-            else if(decide == "n")
+                NewPerson();
+                Console.WriteLine("Would you like to add another person? y for yes, n for no.");
+                decide = Console.ReadLine();
+            } while (decide == "y");
+            if (decide == "n")
             {
+                Console.WriteLine("Exiting program...");
                 //-- https://stackoverflow.com/questions/5682408/command-to-close-an-application-of-console --//
                 Environment.Exit(0);
             }
-            //var people = new List<Person>();
-            //NewPerson();
-            //Console.WriteLine("Would you like to add another person? y for yes, n for no.");
-            //string decide = Console.ReadLine();
 
-            //if(decide == "y")
-            //{
-            //    personCounter += 1;
-            //    NewPerson();
-            //}
-            //else if(decide == "n")
-            //{
-            //    Console.WriteLine("Exiting program...");
-            //    //-- https://stackoverflow.com/questions/5682408/command-to-close-an-application-of-console --//
-            //    Environment.Exit(0);
-            //}
-
-            //void NewPerson()
-            //{
-            //    Console.WriteLine("Enter your first name: ");
-            //    string firstName = Console.ReadLine();
-            //    Console.WriteLine("Enter your last name: ");
-            //    string lastName = Console.ReadLine();
-            //    Person personCounter = new Person(firstName, lastName);
-            //    people.Add(new Person(firstName, lastName));
-            //    Console.WriteLine("Enter your year of birth: ");
-            //    personCounter.YearOfBirth = int.Parse(Console.ReadLine());
-            //    Console.WriteLine($"Full Name: {personCounter.GetFullName()}\nAge: {personCounter.GetAge()}");
-            //}
+            void NewPerson()
+            {
+                Console.WriteLine("Enter your first name: ");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter your last name: ");
+                string lastName = Console.ReadLine();
+                personList.Add(person = new Person(firstName, lastName));
+                Console.WriteLine("Enter your year of birth: ");
+                personCounter.YearOfBirth = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Full Name: {personCounter.GetFullName()}\nAge: {personCounter.GetAge()}");
+            }
         }
     }
 }
